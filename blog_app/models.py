b9581506848be1cfd614caddb4f9a0ad4d34cfef
Michael Echevarria <michael.echevarria@live.com>
2022-09-02 22:24:05 -0500

    created models

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=150, blank=False)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
